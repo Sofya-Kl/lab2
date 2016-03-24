ca169df869c53718605343785d4a3177f4bda329
Klepikova Sofya <sofja.klepikowa14@gmail.com>
2016-03-24 22:19:37 +0600

    code style

--- lab2.c
+++ lab2.c
@@ -6,16 +6,16 @@ int main()
 	printf("Enter a, b, c: ");
 	scanf("%lf %lf %lf", &a, &b, &c);
 	d = b * b - 4 * a * c;
-	if (d==0)
+	if (d == 0)
 	{
 		x1 = -b / 2 * a;
-		printf("x1=x2=%.2lf.\n",x1,x1);
+		printf("x1=x2=%.2lf.\n", x1, x1);
 	}
-	if (d<0)
+	if (d < 0)
 	{
 		printf("no solution");
 	}
-	if (d>0)
+	if (d > 0)
 	{
 		x1 = (-b + sqrt(d)) / 2 * a;
 		x1 = (-b - sqrt(d))/ 2 * a;
